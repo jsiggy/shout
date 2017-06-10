e2873db1a472626acebd2dec1ed5c9b7b2825516
john.sigler <john.sigler@gmail.com>
2017-06-09 21:20:40 -0500

    Add initial step definitions

--- src/test/java/training/bdd/Stepdefs.java
+++ src/test/java/training/bdd/Stepdefs.java
@@ -8,19 +8,16 @@ import cucumber.api.java.en.When;
 public class Stepdefs {
     @Given("^Lucy is (\\d+)m from Sean$")
     public void lucy_is_m_from_Sean(int arg1) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
         throw new PendingException();
     }
 
     @When("^Sean shouts \"([^\"]*)\"$")
     public void sean_shouts(String arg1) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
         throw new PendingException();
     }
 
     @Then("^Lucy hears Sean's message$")
     public void lucy_hears_Sean_s_message() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
         throw new PendingException();
     }
 }
